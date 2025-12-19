8812ddb5b15a68bf0feb899583592b5d2b6e16b1
Motasem AbuBaraka <mota6em@gmail.com>
2025-12-19 19:34:10 +0100

    fix(WhoWeAre): enhance image section with layered background elements for improved visual depth

--- components/Home/WhoWeAre.tsx
+++ components/Home/WhoWeAre.tsx
@@ -57,7 +57,6 @@ export default function WhoWeAre() {
               {t("description.mission")}
             </p>
           </motion.div>
-
           {/* IMAGE RIGHT */}
           <motion.div
             initial={{ opacity: 0, scale: 0.95 }}
@@ -66,13 +65,19 @@ export default function WhoWeAre() {
             transition={{ duration: 0.6 }}
             className="w-full max-w-md mt-5 md:mt-0"
           >
-            <Image
-              src="/imgs/home/aboutmit/mit-main-log-no-bg.png"
-              alt="MIT Visual"
-              width={900}
-              height={900}
-              className="w-full max-w-md md:w-full h-auto drop-shadow-xl"
-            />
+            <div className="relative">
+              <Image
+                src="/imgs/home/aboutmit/mit-main-log-no-bg.png"
+                alt="MIT Visual"
+                width={900}
+                height={900}
+                className="w-full max-w-md md:w-full h-auto drop-shadow-xl relative z-10" // Added z-10 to keep image on top
+              />
+
+              <div className="absolute top-0 right-0 w-64 h-64 bg-[#4d93fb] rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob" />
+              <div className="absolute bottom-0 left-0 w-64 h-64 bg-[#11b505] rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob animation-delay-2000" />
+              <div className="absolute top-20 left-20 w-64 h-64 bg-[#f1c34c] rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob animation-delay-4000" />
+            </div>
           </motion.div>
         </div>
 
