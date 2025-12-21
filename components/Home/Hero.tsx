ea8f322aad0cb03ee4e24d66a694391e2003d6c7
Motasem AbuBaraka <mota6em@gmail.com>
2025-12-21 21:31:08 +0100

    fix(Hero): simplify image handling and update hero title for clarity

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import Image from "next/image";
 import { useTranslations } from "next-intl";
 
@@ -9,49 +8,27 @@ interface HeroProps {
 }
 
 export default function Hero({ images }: HeroProps) {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [currentIndexMobile, setCurrentIndexMobile] = useState(0);
   const t = useTranslations("home");
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prev) => (prev + 1) % images.length);
-      setCurrentIndexMobile((prev) => (prev + 2) % images.length);
-    }, 4500);
-    return () => clearInterval(interval);
-  }, [images.length]);
-
-  if (!images || images.length === 0) return null;
-
-  const currentStack = [
-    images[currentIndexMobile % images.length],
-    images[(currentIndexMobile + 1) % images.length],
-  ];
 
   return (
     <div className="relative w-full min-h-[93.5vh] max-h-screen md:min-h-[87vh] md:max-h-[90vh] overflow-hidden">
       {/** * Desktop Background Slider */}
       <div className="hidden lg:block h-full">
-        {images.map((img, index) => (
-          <Image
-            key={img}
-            src={img}
-            alt={`background ${index}`}
-            fill
-            className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-[1200ms] ease-out ${
-              index === currentIndex ? "opacity-100" : "opacity-0"
-            }`}
-            priority={index === 0}
-            style={{ filter: "brightness(0.9)" }}
-          />
-        ))}
+        <Image
+          src={"/imgs/home/hero/picnic2.jpg"}
+          alt="hero"
+          fill
+          className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-[1200ms] ease-out`}
+          priority={true}
+          style={{ filter: "brightness(0.75)" }}
+        />
       </div>
 
       {/** * Mobile Image Stack */}
       <div className="flex flex-col lg:hidden absolute inset-0 w-full h-full">
         <div className="relative flex-1 w-full transition-opacity duration-700 ease-out">
           <Image
-            src={currentStack[0]}
+            src={"/imgs/home/hero/picnic2.jpg"}
             alt="top"
             fill
             className="object-cover"
@@ -69,7 +46,8 @@ export default function Hero({ images }: HeroProps) {
 
         <div className="relative flex-1 w-full transition-opacity duration-700 ease-out">
           <Image
-            src={currentStack[1]}
+            s
+            src={"/imgs/home/hero/picnic.jpg"}
             alt="bottom"
             fill
             className="object-cover"
@@ -79,15 +57,9 @@ export default function Hero({ images }: HeroProps) {
 
       {/** * Content Overlay */}
       <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-3 md:mt-3 md:px-6 space-y-1">
-        <h1 className="text-4xl poppins.className md:text-6xl font-bold relative">
+        <h1 className="text-3xl md:text-5xl font-bold relative">
           <span className="text-[#f1c34c] drop-shadow-[0_0_4px_black]">
-            {t("hero.title").split(" ")[0]}{" "}
-          </span>
-          <span className="text-[#11b505] drop-shadow-[0_0_4px_black]">
-            {t("hero.title").split(" ")[1]}{" "}
-          </span>
-          <span className="text-[#4d93fb] drop-shadow-[0_0_4px_black]">
-            {t("hero.title").split(" ")[2]}
+            {t("hero.title")}
           </span>
         </h1>
 
