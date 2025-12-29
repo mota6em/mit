854bf057eb2d6ed1744d95c28cb9e5b260030e85
Motasem AbuBaraka <mota6em@gmail.com>
2025-12-29 08:38:11 +0100

    chore(events/page): remove unnecessary export of dynamic variable

--- app/[locale]/events/page.tsx
+++ app/[locale]/events/page.tsx
@@ -1,8 +1,6 @@
 import EventsSection from "@/components/Events/EventsSection";
 import Hero from "@/components/Events/Hero";
 
-export const dynamic = "force-dynamic";
-
 export default function EventsHero() {
   return (
     <div className="flex flex-col gap-8 mb-16">
