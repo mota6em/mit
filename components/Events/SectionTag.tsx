d10b08cb04b9a5b9bc9ad10533b4ba58bb19b5f0
Motasem AbuBaraka <mota6em@gmail.com>
2025-12-30 10:08:00 +0100

    feat(SectionTag): add gray color option to SectionTag component

--- components/Events/SectionTag.tsx
+++ components/Events/SectionTag.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-type SectionTagColor = "green" | "gold" | "blue";
+type SectionTagColor = "green" | "gold" | "blue" | "gray";
 
 interface SectionTagProps {
   text: string;
@@ -12,6 +12,7 @@ export const SectionTag = ({ text, color = "blue" }: SectionTagProps) => {
     green: "bg-green-100 text-green-600",
     gold: "bg-yellow-100 text-yellow-600",
     blue: "bg-blue-100 text-blue-600",
+    gray: "bg-gray-100 text-gray-600",
   };
 
   return (
